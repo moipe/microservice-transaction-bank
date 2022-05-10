06426ecbf4c039057538b0ce0d7f51746120a0ed
Jesus Gian Marco Castro Morales <jcasmora@emeal.nttdata.com>
2022-05-09 22:30:14 -0500

    commit transaction

--- src/main/java/com/nttdata/bank/transaction/model/Transaction.java
+++ src/main/java/com/nttdata/bank/transaction/model/Transaction.java
@@ -3,6 +3,9 @@ package com.nttdata.bank.transaction.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,6 +24,7 @@ public class Transaction {
 	private Double amount;
 	
 	//@JsonIgnore
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private String accountId;
 
 }
