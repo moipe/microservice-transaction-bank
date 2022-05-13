ec3b11dce3c525c4a3feb051cdd96de8e4631b22
Jesus Gian Marco Castro Morales <jcasmora@emeal.nttdata.com>
2022-05-13 11:10:48 -0500

    tercer commit transaction

--- src/main/java/com/nttdata/bank/transaction/controller/TransactionController.java
+++ src/main/java/com/nttdata/bank/transaction/controller/TransactionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/transaction")
+//@RequestMapping("/transaction")
 public class TransactionController {
 	
 	@Autowired
